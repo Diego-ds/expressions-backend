e3345490a47bda6eb33ad045de5badcaa6d0caaa
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2022-12-11 15:40:56 -0500

    first commit

---
+++ src/main/java/com/perficient/expressions/ExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.expressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/expressions/ExpressionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.expressions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
