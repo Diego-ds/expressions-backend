ff7d3e8823c5619150ed972609fb9d25753e35a4
Marco <macroxx120@gmail.com>
2022-12-19 17:11:12 -0500

    jacoco coverage report generated

--- src/main/java/com/perficient/expressions/restcontroller/implementation/RowControllerImp.java
+++ src/main/java/com/perficient/expressions/restcontroller/implementation/RowControllerImp.java
@@ -3,25 +3,51 @@ package com.perficient.expressions.restcontroller.implementation;
 import java.util.List;
 
 import org.bson.json.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.client.FindIterable;
 import com.perficient.expressions.model.RuleQuery;
 import com.perficient.expressions.restcontroller.interfaces.IRowController;
+import com.perficient.expressions.services.interfaces.IRowService;
+import org.bson.Document;
 
 @RestController
+@RequestMapping("/api/v1/rows")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE, RequestMethod.PUT})
 public class RowControllerImp implements IRowController {
 
+    @Autowired
+    private IRowService rowService;
+
     @Override
-    public ResponseEntity<List<JsonObject>> findAll() {
+    @GetMapping("/")
+    public ResponseEntity<FindIterable<Document>> findAll() {
         // TODO Auto-generated method stub
-        return null;
+        return ResponseEntity.ok().body(rowService.findAll());
     }
 
     @Override
-    public ResponseEntity<List<JsonObject>> applyQuery(RuleQuery query) {
+    @PostMapping("/")
+    public ResponseEntity<FindIterable<Document>> applyQuery(RuleQuery query) {
         // TODO Auto-generated method stub
-        return null;
+        FindIterable<Document> response = null;
+
+        try{
+            response = rowService.applyQuery(query.getRule());
+        }catch(IllegalArgumentException e){
+            ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        return ResponseEntity.ok().body(response);
     }
+
+
     
 }
--- src/main/java/com/perficient/expressions/restcontroller/interfaces/IRowController.java
+++ src/main/java/com/perficient/expressions/restcontroller/interfaces/IRowController.java
@@ -6,10 +6,12 @@ import org.bson.json.JsonObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.mongodb.client.FindIterable;
 import com.perficient.expressions.model.RuleQuery;
+import org.bson.Document;
 
 public interface IRowController {
-    public ResponseEntity<List<JsonObject>> findAll();
+    public ResponseEntity<FindIterable<Document>> findAll();
     //to-do crear objeto custom para agregar reglas 
-    public ResponseEntity<List<JsonObject>> applyQuery(@RequestBody RuleQuery query);
+    public ResponseEntity<FindIterable<Document>> applyQuery(@RequestBody RuleQuery query);
 }
